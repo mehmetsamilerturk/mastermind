d3b120296ff6d71837dafd4a028dd2c52b1fc150
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2021-12-16 00:04:03 +0300

    can store correct numbers in correct locations in an array

--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,21 @@
 class Computer
-  attr_reader :numbers, :same_numbers
+  attr_reader :numbers, :same_numbers, :correct_position, :index_code, :index_guess
+
   def initialize
     @numbers = []
-    @same_numbers = []
+    @correct_position = []
+    @index_code = {}
+    @index_guess = {}
   end
+
   def generate_code
     number = Random.new
-    for i in (1..4)
+    (1..4).each do |_i|
       @numbers.push(number.rand(1..6))
     end
     @numbers = @numbers.join
   end
+
   def compare(human)
     code_split = @numbers.split('')
     guess_split = human.guess.split('')
@@ -18,11 +23,28 @@ class Computer
     @same_numbers = guess_split.select do |num_guess|
       code_split.include?(num_guess)
     end
+
+    code_split.each_with_index do |num, index|
+      @index_code[index] = num
+      @index_code
+    end
+
+    guess_split.each_with_index do |num, index|
+      @index_guess[index] = num
+      @index_guess
+    end
+
+    @index_code.each do |key_code, value_code|
+      @index_guess.each do |key_guess, value_guess|
+        @correct_position.push(value_guess) if key_code == key_guess && value_code == value_guess
+      end
+    end
   end
 end
 
 class Human
   attr_reader :guess
+
   def guess_color
     @guess = gets.chomp
   end
@@ -34,8 +56,8 @@ human = Human.new
 computer.generate_code
 p computer.numbers
 
-puts "enter a number: "
+puts 'enter a number: '
 human.guess_color
 computer.compare(human)
-p computer.same_numbers.flatten.compact
-
+p computer.same_numbers
+p computer.correct_position
