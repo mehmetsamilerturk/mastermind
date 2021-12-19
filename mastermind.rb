337bcfacb1d3361a5cb07eaeff6f4e64ba90adcf
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2021-12-19 22:52:21 +0300

    computer now has the ability to guess, randomly

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,24 @@
+module FormatOutput
+  def format_output(player)
+    player.guess.split('').map do |e|
+      case e
+      when '1'
+        e = '1'.bg_blue
+      when '2'
+        e = '2'.bg_cyan
+      when '3'
+        e = '3'.bg_green
+      when '4'
+        e = '4'.bg_magenta
+      when '5'
+        e = '5'.bg_red
+      when '6'
+        e = '6'.bg_yellow
+      end
+    end
+  end
+end
+
 class String
   # colorization
   def colorize(color_code)
@@ -58,12 +79,16 @@ class String
 end
 
 class Computer
+  include FormatOutput
+
   attr_reader :numbers, :correct_position, :wrong_position, :code_remainder, :guess_remainder
+  attr_accessor :guess
 
   def initialize
     @numbers = []
     @correct_position = []
     @wrong_position = []
+    @guess = []
   end
 
   def generate_code
@@ -115,17 +140,76 @@ class Computer
       end
     end
   end
+
+  def guess_color
+    number = Random.new
+
+    (1..4).each do |_i|
+      @guess.push(number.rand(1..6))
+    end
+    @guess = @guess.join
+  end
 end
 
 class Human
-  attr_reader :guess, :secret_code
+  include FormatOutput
+
+  attr_reader :guess, :secret_code, :correct_position, :wrong_position, :code_remainder, :guess_remainder
+
+  def initialize
+    @correct_position = []
+    @wrong_position = []
+
+  end
+
+  def create_code
+    @secret_code = gets.chomp
+  end
 
   def guess_color
     @guess = gets.chomp
   end
 
-  def create_code
-    @secret_code = gets.chomp
+  def compare(computer)
+    code_split = @secret_code.split('')
+    guess_split = computer.guess.split('')
+
+    code_split.each_with_index do |code_val, code_index|
+      guess_split.each_with_index do |guess_val, guess_index|
+        @correct_position.push(guess_val) if code_index == guess_index && code_val == guess_val
+      end
+    end
+
+    @code_remainder = code_split.dup
+    @guess_remainder = guess_split.dup
+
+    @correct_position.all? do |n|
+      idx = @code_remainder.index(n)
+
+      return false if idx.nil?
+
+      @code_remainder.delete_at(idx)
+    end
+
+    @correct_position.all? do |n|
+      idx = @guess_remainder.index(n)
+
+      return false if idx.nil?
+
+      @guess_remainder.delete_at(idx)
+    end
+
+    @code_remainder.each do |code_val|
+      @guess_remainder.each do |guess_val|
+        next unless code_val == guess_val
+
+        @wrong_position.push(guess_val)
+
+        @guess_remainder.delete_at(@guess_remainder.index(guess_val))
+
+        break
+      end
+    end
   end
 end
 
@@ -157,32 +241,36 @@ if choice == '2'
     correct_cpy.replace(Array.new(size_correct, 'O'.green))
     wrong_cpy.replace(Array.new(size_wrong, 'O'.red))
 
-    formatted_outout = human.guess.split('').map do |e|
-      case e
-      when '1'
-        e = '1'.bg_blue
-      when '2'
-        e = '2'.bg_cyan
-      when '3'
-        e = '3'.bg_green
-      when '4'
-        e = '4'.bg_magenta
-      when '5'
-        e = '5'.bg_red
-      when '6'
-        e = '6'.bg_yellow
-      end
-    end
-
-    puts "#{formatted_outout.join(' ')}  Clues: #{(correct_cpy + wrong_cpy).join}"
+    puts "#{human.format_output(human).join(' ')}  Clues: #{(correct_cpy + wrong_cpy).join}"
     computer.correct_position.clear
     computer.wrong_position.clear
+
   end
 elsif choice == '1'
   puts "colors: #{'1'.bg_blue} #{'2'.bg_cyan} #{'3'.bg_green} #{'4'.bg_magenta} #{'5'.bg_red} #{'6'.bg_yellow}"
-  print "Enter your 4 digit secret code between these colors(1-6)> "
+  print 'Enter your 4 digit secret code between these colors(1-6)> '
   human.create_code
-  p human.secret_code
+
+  until computer.guess == human.secret_code
+    computer.guess_color
+
+    human.compare(computer)
+
+    size_correct = human.correct_position.size
+    size_wrong = human.wrong_position.size
+
+    correct_cpy = human.correct_position.dup
+    wrong_cpy = human.wrong_position.dup
+
+    correct_cpy.replace(Array.new(size_correct, 'O'.green))
+    wrong_cpy.replace(Array.new(size_wrong, 'O'.red))
+
+    puts "#{computer.format_output(computer).join(' ')}  Clues: #{(correct_cpy + wrong_cpy).join}"
+
+    human.correct_position.clear
+    human.wrong_position.clear
+    computer.guess = []
+  end
 else
-  puts "Invalid input"
+  puts 'Invalid input'
 end
